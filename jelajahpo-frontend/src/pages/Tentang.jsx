67ef1c1b12ac20cd2194947ae9a52a3424ea5ccf
ceisyadevi <deviceisya@gmail.com>
2026-08-31 11:41:49 +0700

    tentang

---
+++ jelajahpo-frontend/src/pages/Tentang.jsx
@@ -0,0 +1,16 @@
+export default function Tentang() {
+return (
+<div className="row text-start">
+<div className="col-10">
+<h2 className="fw-bold mb-3">Tentang Saya</h2>
+<h4>haiiiiii aku ceisaa</h4>
+<p>
+XII RPL 1
+</p>
+<p>
+Alasan utama menyukai skincare adalah karena kegiatan ini merawat kesehatan kulit, memberi waktu untuk diri sendiri, dan membua rasa percaya diri meningkat.
+</p>
+</div>
+</div>
+);
+}
