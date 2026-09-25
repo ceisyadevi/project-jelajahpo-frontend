103cf932f55bfdf84386d0bde2f414c9950ee0af
ceisyadevi <deviceisya@gmail.com>
2026-09-25 10:56:44 +0700

    katgeri

--- jelajahpo-frontend/src/pages/Kategori.jsx
+++ jelajahpo-frontend/src/pages/Kategori.jsx
@@ -1,3 +1,58 @@
+import { useEffect, useState } from "react";
+
 export default function Kategori() {
-    return <h2>Halaman Kategori</h2>;
+    const [kategori, setKategori] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState("");
+
+    const getKategori = async () => {
+        try {
+            const res = await fetch("http://localhost:3001/kategori");
+            if (!res.ok) {
+                throw new Error("Gagal mengambil data kategori");
+            }
+            const data = await res.json();
+            setKategori(data);
+        } catch (err) {
+            setError(err.message);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        getKategori();
+    }, []);
+
+    if (loading) {
+        return <div className="container mt-4">Loading...</div>
+    }
+
+    if (error) {
+        return (
+            <div className="container mt-4 text-danger">{error}</div>
+        );
+    }
+
+    return (
+       <div className="container mt-4">
+        <h2>Data Kategori</h2>
+        <table className="table table-bordered table-striped mt-3">
+            <thead>
+                <tr>
+                    <th>ID Kategori</th>
+                    <th>Nama Kategori</th>
+                </tr>
+            </thead>
+            <tbody>
+                {kategori.map((item) => (
+                    <tr key={item.id_kategori}>
+                        <td>{item.id_kategori}</td>
+                        <td>{item.kategori}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+       </div> 
+    );
 }
